67ab6dba81db97730b2a816a168797985c4093db
ist182069 <jose.bras@tecnico.ulisboa.pt>
2019-07-12 18:45:57 +0100

    Demo Java Program with basic queries (fetch all queries)

--- db/src/main/java/inesc_id/gsd/bibtrek/ExampleApp.java
+++ db/src/main/java/inesc_id/gsd/bibtrek/ExampleApp.java
@@ -33,8 +33,11 @@ public class ExampleApp {
     
     private static final String CREATE_GRAPH_DATABASE = "1";
     private static final String DELETE_GRAPH_DATABASE = "2";
-    private static final String QUERY_GRAPH_DATABASE = "3";
-    private static final String EXIT = "4";
+    private static final String GET_ALL_AUTHORS = "3";
+    private static final String GET_ALL_PAPERS = "4";
+    private static final String GET_ALL_INSTITUTIONS = "5";
+    private static final String GET_ALL_SUBJECTS = "6";
+    private static final String EXIT = "0";
     
     private static GraphDatabaseService graphDb;
     
@@ -122,7 +125,130 @@ public class ExampleApp {
 		
 		System.out.println("Success: the database was successfully deleted!");
 		System.out.println(""); 
-    }          
+    }   
+    
+    // getAuthors(): Simply allows the user to pick all authors from the database
+    public void getAuthors() {
+    	String attribute, fname = "", surname = "", node = "", rows = ""; 
+    	
+    	try ( Transaction ignored = graphDb.beginTx();
+             Result result = graphDb.execute( "MATCH (authors:Author) RETURN authors.surname, authors.fname, authors" ) )
+    	{
+    		while ( result.hasNext() )
+    		{
+    			Map<String,Object> row = result.next();
+                for ( Entry<String,Object> column : row.entrySet() )
+                {	
+                	attribute = column.getKey();
+                	switch(attribute) {
+                		case "authors.fname":
+                			fname = (String) column.getValue();
+                			break;
+                		case "authors.surname":
+                			surname = (String) column.getValue();      
+                			break;
+                		default:                			
+                			node = column.getValue() + "";
+                	}
+                }
+                rows += node + " :  " + fname + " " + surname + "\n";
+           }
+    	}
+    	
+    	System.out.println(rows);
+    }
+    
+    // getInstitutions(): Simply allows the user to pick all papers from the database
+    public void getInstitutions() {
+    	String attribute, iname = "", node = "", rows = ""; 
+    	
+    	try ( Transaction ignored = graphDb.beginTx();
+             Result result = graphDb.execute( "MATCH (institutions:Institution) RETURN institutions, institutions.iname" ) )
+    	{
+    		while ( result.hasNext() )
+    		{
+    			Map<String,Object> row = result.next();
+                for ( Entry<String,Object> column : row.entrySet() )
+                {	
+                	attribute = column.getKey();
+                	switch(attribute) {
+                		case "institutions.iname":
+                			iname = (String) column.getValue();
+                			break;                		
+                		default:                			
+                			node = column.getValue() + "";
+                	}
+                }
+                rows += node + " :  " + iname + "\n";
+           }
+    	}
+    	
+    	System.out.println(rows);
+    }
+    
+    // getPapers(): Simply allows the user to pick all papers from the database
+    public void getPapers() {
+    	String attribute, year = "", title = "", library = "", node = "", rows = ""; 
+    	
+    	try ( Transaction ignored = graphDb.beginTx();
+             Result result = graphDb.execute( "MATCH (papers:Paper) RETURN papers, papers.title, papers.library, papers.year" ) )
+    	{
+    		while ( result.hasNext() )
+    		{
+    			Map<String,Object> row = result.next();
+                for ( Entry<String,Object> column : row.entrySet() )
+                {	
+                	attribute = column.getKey();
+                	switch(attribute) {
+	                	case "papers.library":
+	            			library = (String) column.getValue();
+	            			break;
+	                	case "papers.title":
+	            			title = (String) column.getValue();
+	            			break;
+                		case "papers.year":
+                			year = Long.toString((long) column.getValue());
+                			break;                		
+                		default:  
+                			if(attribute.contains("Node")) {
+                				node = column.getValue() + "";
+                			}
+                	}
+                }
+                rows += node + " :  [T] " + title + " [L] " + library + " [Y] " + year + "\n";
+           }
+    	}
+    	
+    	System.out.println(rows);
+    }
+    
+    // getInstitutions(): Simply allows the user to pick all papers from the database
+    public void getSubjects() {
+    	String attribute, subject = "", node = "", rows = ""; 
+    	
+    	try ( Transaction ignored = graphDb.beginTx();
+             Result result = graphDb.execute( "MATCH (subjects:Subject) RETURN subjects, subjects.subject" ) )
+    	{
+    		while ( result.hasNext() )
+    		{
+    			Map<String,Object> row = result.next();
+                for ( Entry<String,Object> column : row.entrySet() )
+                {	
+                	attribute = column.getKey();
+                	switch(attribute) {
+                		case "subjects.subject":
+                			subject = (String) column.getValue();
+                			break;                		
+                		default:                			
+                			node = column.getValue() + "";
+                	}
+                }
+                rows += node + " :  " + subject + "\n";
+           }
+    	}
+    	
+    	System.out.println(rows);
+    }
     
     private static void registerShutdownHook( final GraphDatabaseService graphDb )
     {
@@ -154,11 +280,13 @@ public class ExampleApp {
         
         @SuppressWarnings("resource")
 		Scanner clientInputScanner = new Scanner(System.in);
-
+        /*
         System.out.print("Insert the (BOLT) port on which your local graph database is running: ");
         String localport = clientInputScanner.nextLine();
-
+        
 		ExampleApp queryAgent = new ExampleApp(localport);    
+        */
+        ExampleApp queryAgent = new ExampleApp("7687");
         
         boolean databaseCreated = false;
         queryAgent.deleteDatabase();
@@ -174,9 +302,15 @@ public class ExampleApp {
             System.out.println("");
             System.out.println("(2): Delete the example database.");
             System.out.println("");
-            System.out.println("(3): Query the database.");
+            System.out.println("(3): Get all authors.");
             System.out.println("");
-            System.out.println("(4): Exits the program.");
+            System.out.println("(4): Get all papers.");
+            System.out.println("");
+            System.out.println("(5): Get all institutions.");
+            System.out.println("");
+            System.out.println("(6): Get all subjects.");
+            System.out.println("");
+            System.out.println("(0): Exits the program.");
             System.out.println("");
             System.out.println("===================================");
             System.out.println("");
@@ -192,7 +326,6 @@ public class ExampleApp {
 	                    queryAgent.createExampleDatabase();
 	                    databaseCreated = true;
                 	} else {
-                		System.out.println("");
                 		System.out.println("Error: the database is already created!");
                 		System.out.println("");
                 	}
@@ -203,13 +336,40 @@ public class ExampleApp {
 	                    databaseCreated = false;
 	                    break;
                 	} else {
-                		System.out.println("");
                 		System.out.println("Error: the database has not been created.");
                 		System.out.println("");
                 	}
-                case QUERY_GRAPH_DATABASE:
-                    System.out.println("Introduce a query: ");
-                    String query = clientInputScanner.nextLine();
+                case GET_ALL_AUTHORS:
+                	if(databaseCreated == true) {
+	                    queryAgent.getAuthors();
+                	} else {
+                		System.out.println("Error: the database is not created!");
+                		System.out.println("");
+                	}
+                    break;
+                case GET_ALL_PAPERS:
+                	if(databaseCreated == true) {
+	                    queryAgent.getPapers();
+                	} else {
+                		System.out.println("Error: the database is not created!");
+                		System.out.println("");
+                	}
+                    break;
+                case GET_ALL_INSTITUTIONS:
+                	if(databaseCreated == true) {
+	                    queryAgent.getInstitutions();
+                	} else {
+                		System.out.println("Error: the database is not created!");
+                		System.out.println("");
+                	}
+                    break;
+                case GET_ALL_SUBJECTS:
+                	if(databaseCreated == true) {
+	                    queryAgent.getSubjects();
+                	} else {
+                		System.out.println("Error: the database is not created!");
+                		System.out.println("");
+                	}
                     break;
                 case EXIT:
                     return;   
